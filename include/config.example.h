5a0261df02c23228ccc56c217bc20e9f150d690c
rashidasp28 <rashidasp28@gmail.com>
2026-05-15 09:49:45 +0200

    Add firmware configuration template

---
+++ include/config.example.h
@@ -0,0 +1,15 @@
+#pragma once
+
+// Copy this file to config.h for real deployments.
+// Do not commit secrets.
+
+#define WIFI_SSID "YOUR_WIFI_NAME"
+#define WIFI_PASSWORD "YOUR_WIFI_PASSWORD"
+
+#define MQTT_HOST "broker.hivemq.com"
+#define MQTT_PORT 1883
+#define MQTT_TOPIC "climasense/ghana/northern/readings"
+
+#define DEVICE_ID "CS-GH-NR-0001"
+#define SCHOOL_ID "PILOT-SCHOOL-001"
+#define COMMUNITY_NAME "Tamale"
