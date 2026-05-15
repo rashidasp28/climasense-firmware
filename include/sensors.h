9b8608c36d0c7d56c610522bf20b8428dd80859a
rashidasp28 <rashidasp28@gmail.com>
2026-05-15 09:52:11 +0200

    Extend climate reading structure

--- include/sensors.h
+++ include/sensors.h
@@ -3,11 +3,13 @@
 struct ClimateReading {
   float temperatureC;
   float humidityPercent;
+  float heatIndexC;
   float pm25;
   float pm10;
   float soilMoisturePercent;
   float rainfallMm;
   float batteryVoltage;
+  bool dhtHealthy;
 };
 
 void initializeSensors();
